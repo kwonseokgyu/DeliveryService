b26a2ea0112afa8f9e85f22217bf9997ce7ad0bc
kwonseokgyu <gsg4821@gmail.com>
2025-10-27 16:30:14 +0900

    ㄴㅇ

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 // import './index.css'
-import App from './App.jsx';
+import App from "./App.jsx";
 
-import { Provider } from 'react-redux';
-import store from './store/index.js';
+import { Provider } from "react-redux";
+import store from "./store/index.js";
 
-createRoot(document.getElementById('root')).render(
-    <StrictMode>
-        <Provider store={store}>
-            <App />
-        </Provider>
-    </StrictMode>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
 );
